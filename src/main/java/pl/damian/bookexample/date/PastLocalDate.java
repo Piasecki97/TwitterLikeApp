e2990a040459f46b02dfdaf86ed3661bf03e2db2
Damian Piasecki <piasecki9712@gmail.com>
2018-05-08 13:43:46 +0200

    Date verification

--- src/main/java/pl/damian/bookexample/date/PastLocalDate.java
+++ src/main/java/pl/damian/bookexample/date/PastLocalDate.java
@@ -1,4 +1,25 @@
 package pl.damian.bookexample.date;
 
-public class PastLocalDate {
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+import java.time.LocalDate;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PastLocalDate.PastValidator.class)
+@Documented
+public @interface PastLocalDate {
+    String message() default "{javax.validation.constraints.Past.message}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    class PastValidator implements ConstraintValidator<PastLocalDate, LocalDate>{
+        public void initialize(PastLocalDate past) {
+        }
+        public boolean isValid(LocalDate localDate, ConstraintValidatorContext context) {
+            return localDate == null || localDate.isBefore(LocalDate.now());
+        }
+    }
 }
